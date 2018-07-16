5227b03ccc6f1486245b621f531a95939da5ea45
Darren Carlin <darrencarlin@Darrens-MBP.home>
2018-07-16 03:33:40 -0400

    lots of changes (counters / revamped projects section)

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     // Copyright Date
-
     var date = new Date();
     document.getElementById("year").innerHTML = date.getFullYear();
-
     // Scroll
-
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
+            document.querySelector(this.getAttribute(
+                'href')).scrollIntoView({
                 alignToTop: false,
                 behavior: "smooth",
                 block: "start",
@@ -18,16 +15,48 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         });
     });
-
     // Fade in 
-
-    let elements = document.querySelectorAll("header, nav, .image, .about, .work, .contact");
-
-    elements.forEach((elem) =>{
+    let elements = document.querySelectorAll(
+        "header, nav, .image, .about, .work, .contact, .counter");
+    elements.forEach((elem) => {
         elem.style.opacity = "1";
         elem.style.bottom = "0";
-    }) 
+    });
+    // Counters 
+    let run = true;
+    const counterpos = document.getElementById('counters').offsetTop -
+        500;
+    window.addEventListener('scroll', () => {
+        const scrollPos = window.scrollY || window.scrollTop ||
+            document.getElementsByTagName("html")[0].scrollTop;
+        if (scrollPos >= counterpos) {
+            if (run) {
+                counters();
+            }
+            run = false;
+        }
+    });
 
-
- 
-}, false); 
+    function counters() {
+        const options = {  
+            useEasing: true,
+              useGrouping: true,
+              separator: ',',
+              decimal: '.',
+        };
+        const html = new CountUp(document.getElementById('htmlCounter'),
+            0, 6234, 0, 3, options);
+        const sass = new CountUp(document.getElementById('sassCounter'),
+            0, 7033, 0, 3, options);
+        const js = new CountUp(document.getElementById('jsCounter'), 0,
+            5352, 0, 3, options);
+        const vue = new CountUp(document.getElementById('vueCounter'),
+            0, 1658, 0, 3, options);
+        if (!html.error || !sass.error || !js.error) {  
+            html.start();
+            sass.start();
+            js.start();
+            vue.start();
+        }
+    }
+}, false);
